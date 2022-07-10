e806b18e6f8b913d9c5fdbd3154302eab4292ff0
jake unruh <jake.unruh@gmail.com>
2022-07-10 02:47:07 -0500

    more styling of heade elems

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,11 @@
 import "../../styles/header.scss"
 import { useScrollDirection } from "../../hooks/use-scroll-direction"
-import Logo from "../../img/logo.svg"
 
 function Header() {
   // renaming the return of useScrollDirection to use in the header to set the class
   const scrollDirection = useScrollDirection();
   return (
     <header className={`header ${ scrollDirection === 'down' ? 'header--hide' : 'header--show'}`}>
-      <div className={`header__logo ${ scrollDirection === 'down' ? '' : 'header__logo--show'}`}>
-        <img src={Logo} alt="" className="logo" />
-      </div>
       <h1 className="header__title">
         <span className="header__span--vertical">Jake</span>Unruh 
       </h1>
